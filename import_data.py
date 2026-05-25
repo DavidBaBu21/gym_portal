87d0d0fbe51a6d1b3077b1d499ceb0dd2787138f
David Bailón Bustos <davidbailonbustos@outlook.com>
2026-05-24 22:14:27 -0600

    Update import_data.py

--- import_data.py
+++ import_data.py
@@ -3,9 +3,12 @@ from sqlalchemy import text
 
 def import_sql():
     with open("backup.sql", "r", encoding="utf-8") as f:
-        sql_commands = f.read()
+        sql_commands = f.read().split(";\n")  # separa por cada sentencia
         with db.engine.connect() as conn:
-            conn.execute(text(sql_commands))
+            for command in sql_commands:
+                cmd = command.strip()
+                if cmd:  # evita líneas vacías
+                    conn.execute(text(cmd))
 
 if __name__ == "__main__":
     import_sql()
